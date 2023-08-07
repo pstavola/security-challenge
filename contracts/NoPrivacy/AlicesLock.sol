6d41c0c2240278a01031220cf275470a80c39e7d
Patrizio Stavola <patrizio.stavola@gmail.com>
2023-08-07 12:42:39 +0200

    Fixed Challenge 1

--- contracts/NoPrivacy/AlicesLock.sol
+++ contracts/NoPrivacy/AlicesLock.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.8.0;
 
 contract AlicesLock {
   bool private locked;
-  bytes32 password;
+  // @fix using keccak hash of the password instead of plain text
+  bytes32 hashed_password;
 
   constructor(bytes32 _password) {
     locked = true;
-    password = _password;
+    hashed_password = _password;
   }
 
   function ifLocked() public view returns(bool){
     return locked;
   }
 
-  function unlock(bytes32 _password) public {
-    if ( password == _password) {
+  function unlock(bytes memory _password) public {
+    if ( hashed_password == keccak256(_password) ) {
       locked = false;
     }
   }
--- test/no-privacy.js
+++ test/no-privacy.js
@@ -1,5 +1,8 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");  // Allows to pass string as parameter to keccak256
+
 require("dotenv").config();
 
 describe("No Privacy Challenge", function () {
@@ -12,7 +15,10 @@ describe("No Privacy Challenge", function () {
       "AlicesLock",
       alice
     );
-    this.lock = await NoPrivacyFactory.deploy(process.env.PASSWORD);
+    //this.lock = await NoPrivacyFactory.deploy(process.env.PASSWORD);
+    // @fix using keccak hash of the password instead of plain text
+    const hashed_pw = keccak256(utf8ToBytes("process.env.PASSWORD"));
+    this.lock = await NoPrivacyFactory.deploy(hashed_pw);
     // Peaking into the env is considered cheating!
   });
 
@@ -21,6 +27,8 @@ describe("No Privacy Challenge", function () {
     // password is stored in contract bytecode memory storage 
     let password = ethers.provider.getStorageAt(this.lock.address, 1);
     await this.lock.unlock(password);
+    // make it work
+    //await this.lock.unlock(utf8ToBytes("process.env.PASSWORD"));
   });
 
   after(async function () {
