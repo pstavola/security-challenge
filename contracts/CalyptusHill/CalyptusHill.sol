34df8c52b8d5d436845750729b051cb072062767
Patrizio Stavola <patrizio.stavola@gmail.com>
2023-08-07 14:21:56 +0200

    Fixed Challenge 3

--- contracts/CalyptusHill/CalyptusHill.sol
+++ contracts/CalyptusHill/CalyptusHill.sol
@@ -6,6 +6,8 @@ contract CalyptusHill {
   address public atTheTop;
   uint public bribe;
   address public owner;
+  // @fix using pull model
+  mapping (address => uint256) public balanceToWithdraw;
 
   constructor(address _atTheTop) payable {
     owner = msg.sender;  
@@ -15,9 +17,16 @@ contract CalyptusHill {
 
   receive() external payable {
     require(msg.value >= bribe || msg.sender == owner);
-    payable(atTheTop).transfer(msg.value);
+    //payable(atTheTop).transfer(msg.value);
+    // @fix using pull model
+    balanceToWithdraw[atTheTop] += bribe;
     atTheTop = msg.sender;
     bribe = msg.value;
+  }
+
+  // @fix using pull model
+  function withdraw() public {
+    payable(msg.sender).transfer(balanceToWithdraw[atTheTop]);
   }
 
 }
